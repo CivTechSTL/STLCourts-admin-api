94ac4b66e2977a1bd76fb13332c82354e692dbd8
ctwoolsey <clark.woolsey@gmail.com>
2017-05-24 09:10:10 -0500

    Partial Initial Commit

---
+++ src/main/java/svc/profiles/LocalWithHSQLAccessProfile.java
@@ -0,0 +1,34 @@
+package svc.profiles;
+
+import java.sql.SQLException;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+
+import org.hsqldb.util.DatabaseManagerSwing;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+@Profile("localWithHSQLAccess")
+public class LocalWithHSQLAccessProfile {
+	@Bean
+	public DataSource dataSource() {
+		EmbeddedDatabaseBuilder dbBuilder = new EmbeddedDatabaseBuilder();
+		return dbBuilder.setType(EmbeddedDatabaseType.HSQL)
+				.build();
+	}
+	
+	//default db: testdb, username : sa, password : ''
+		@PostConstruct
+		public void getDbManager() throws SQLException{
+			DatabaseManagerSwing manager = new DatabaseManagerSwing();
+			manager.main();
+			manager.start();
+		}
+	
+}
